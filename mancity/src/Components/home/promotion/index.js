6e627642dccbaa66c57176fbab8a178ab46c6b44
unknown <catalin.binzari99@gmail.com>
2020-07-18 16:45:21 +0300

    enroll enroll reveal component

---
+++ mancity/src/Components/home/promotion/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PromotionAnimatiom from "./Animation";
+const Promotion = () => {
+  return (
+    <div className="promotion_wrapper" style={{ background: "#ffffff" }}>
+      <div className="container">
+        <PromotionAnimatiom />
+      </div>
+    </div>
+  );
+};
+
+export default Promotion;
